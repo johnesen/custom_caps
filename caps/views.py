a6c34410c9787fb36c606cb1c39b188f7c81cfaf
NurJ0hn <nurjantunteev@gmail.com>
2022-03-23 18:36:49 +0600

    acces for list view

--- caps/views.py
+++ caps/views.py
@@ -30,7 +30,7 @@ class CapsListView(generics.ListAPIView):
     """
     queryset = Caps.available.all()
     serializer_class = CapsListViewSerializers
-    permission_classes = [permissions.AllowAny]
+    # permission_classes = [permissions.AllowAny]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['name', 'brand__name', 'price', 'brand__name']
     ordering_fields = 'price created_data'.split()
@@ -42,8 +42,8 @@ class CapsCreateView(generics.CreateAPIView):
     """
     queryset = Caps.available.all()
     serializer_class = CapsCreateViewSerializers
-    permission_classes = [
-        permissions.IsAdminUser, permissions.IsAuthenticated]
+    # permission_classes = [
+    #     permissions.IsAdminUser, permissions.IsAuthenticated]
 
 
 class BrandListView(generics.ListAPIView):
@@ -52,8 +52,8 @@ class BrandListView(generics.ListAPIView):
     """
     queryset = Brand.objects.all()
     serializer_class = BrandListSerializer
-    permission_classes = [
-        permissions.AllowAny]
+    # permission_classes = [
+        # permissions.AllowAny]
 
 
 class BrandCapListAPIView(ListModelMixin, GenericAPIView): 
@@ -62,8 +62,8 @@ class BrandCapListAPIView(ListModelMixin, GenericAPIView):
     """
     # queryset = Caps.available.all()
     serializer_class = CapsListViewSerializers
-    permission_classes = [
-        permissions.AllowAny]
+    # permission_classes = [
+    #     permissions.AllowAny]
     def get_queryset(self):
         slug_url = self.kwargs['slug_url']
         return Caps.available.filter(brand__slug=slug_url)
